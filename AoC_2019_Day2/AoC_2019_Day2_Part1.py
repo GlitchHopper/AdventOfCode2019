6f3da192f702d57dafc7720523991bcba0582346
ShaneWoodland <ohmyapple3.14@gmail.com>
2019-12-03 18:32:14 +0900

    Fixed string grammar error and file path string

--- AoC_2019_Day2/AoC_2019_Day2_Part1.py
+++ AoC_2019_Day2/AoC_2019_Day2_Part1.py
@@ -1,4 +1,4 @@
-file = open("/Users/nathanielgugel/Desktop/AdventOfCode2019/AoC_2019_Day2_PuzzleInput.txt", "r")
+file = open("/Users/nathanielgugel/Desktop/AdventOfCode2019/AoC_2019_Day2/AoC_2019_Day2_PuzzleInput.txt", "r")
 
 intCodes = [int(x) for x in file.readline().split(",")]
 
@@ -17,7 +17,7 @@ for index in range(0, len(intCodes), 4):
     elif opCode == 2:
         intCodes[destination] = intCodes[a] * intCodes[b]
     elif opCode == 99:
-        print("The value final value at index 0 is " + str(intCodes[0]))
+        print("The final value at index 0 is " + str(intCodes[0]))
         break
     else:
         print("Error: Invalid OpCode.")
