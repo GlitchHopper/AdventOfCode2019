09cd84085f8a27793b16659a0d9b232f0d0677d4
ShaneWoodland <ohmyapple3.14@gmail.com>
2019-12-09 02:00:16 +0900

    DONE with Day 6 (o_o)

---
+++ Day6/Part2.py
@@ -0,0 +1,41 @@
+file = open("/Users/nathanielgugel/Desktop/AdventOfCode2019/Day6/PuzzleInput")
+
+orbits = {}
+count = 0
+
+
+
+for line in file:
+    primary, satellite = line.strip().split(")")
+    orbits[satellite] = primary
+
+def GetOrbitToCOM(orbits, satellite):
+    
+    distance = 0
+
+    satellite = orbits[satellite] #Gets us past ourselves // prevents OBO
+    path = {satellite : distance}
+
+    while satellite in orbits:
+        satellite = orbits[satellite]
+        distance += 1
+        path[satellite] = distance
+        
+
+    return path
+    
+
+youPath = GetOrbitToCOM(orbits, "YOU")
+sanPath = GetOrbitToCOM(orbits, "SAN")
+
+transferCount = 0
+
+for primary in youPath:
+    if primary in sanPath:
+        transferCount = youPath[primary] + sanPath[primary]
+        break
+
+print("The total number of orbit transfers is " + str(transferCount))
+
+file.close()
+    
